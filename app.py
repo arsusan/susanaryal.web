04bc3605f6d4a92f89741eaa8bf85fdbd2bfff4a
SUSAN ARYAL <77282787+arsusan@users.noreply.github.com>
2024-03-27 21:58:29 +0545

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html"),"assets\profile.profile.jpg"
+
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+@app.route("/index.html")
+def index():
+    return render_template("index.html"),"assets\profile.profile.jpg"
+
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/resume.html")
+def resume():
+    return render_template("resume.html")
+    
+
+if __name__ == "__main__":
+    app.run(debug = True)
